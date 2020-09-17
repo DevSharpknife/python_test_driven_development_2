0535dc37686a08f2aec53eb21e01fabacaac8e9e
svvishjs <andreww92@live.com>
2020-09-17 13:23:39 +0100

    added latest test and latest function

--- start_code/high_scores/tests/high_scores_test.py
+++ start_code/high_scores/tests/high_scores_test.py
@@ -10,11 +10,12 @@ class HighScoresTest(unittest.TestCase):
     def setUp(self):
 
         self.list_1 = [34, 54, 765, 234, 1, 32, 764, 98, 901, 378, 811]
-        
+
     # Tests
-
     # Test latest score (the last thing in the list)
-
+    def test_latest(self):
+        get_latest_score = latest(self.list_1)
+        self.assertEqual(811, get_latest_score)
     # Test personal best (the highest score in the list)
 
     # Test top three from list of scores
