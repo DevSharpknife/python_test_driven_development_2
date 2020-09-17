32194ef8f8d1d79aa3142d70ce5e69d071c985c8
svvishjs <andreww92@live.com>
2020-09-17 13:52:55 +0100

    Sorted hi to low score test added and function passed

--- start_code/high_scores/src/high_scores.py
+++ start_code/high_scores/src/high_scores.py
@@ -10,3 +10,7 @@ def personal_best(scores):
 def personal_top_three(scores):
     sorted_scores = sorted(scores, reverse=True)
     return sorted_scores[0:3]
+
+def hi_to_low_scores(scores):
+     sorted_scores = sorted(scores, reverse=True)
+     return sorted_scores
--- start_code/high_scores/tests/high_scores_test.py
+++ start_code/high_scores/tests/high_scores_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.high_scores import latest, personal_best, personal_top_three
+from src.high_scores import latest, personal_best, personal_top_three, hi_to_low_scores
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v4.0.0
 
@@ -25,7 +25,9 @@ class HighScoresTest(unittest.TestCase):
         get_personal_top_three = personal_top_three(self.list_1)
         self.assertEqual([901, 811, 765], get_personal_top_three)
     # Test ordered from highest tp lowest
-
+    def test_high_score_to_low_score(self):
+        get_hi_to_low_scores = hi_to_low_scores(self.list_1)
+        self.assertEqual([901, 811, 765, 764, 378, 234, 98, 54, 34, 32, 1], get_hi_to_low_scores)
     # Test top three when there is a tie
 
     # Test top three when there are less than three
