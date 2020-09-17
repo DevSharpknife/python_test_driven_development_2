7c731c428d6a4f3584fb1e1c0020ffad9d333841
svvishjs <andreww92@live.com>
2020-09-17 15:23:37 +0100

    written tests for compound interest and functions to pass these

--- start_code/compound_interest/src/compound_interest.py
+++ start_code/compound_interest/src/compound_interest.py
@@ -1,8 +1,19 @@
 class CompoundInterest:
     
     def __init__(self, principal_amount, years, interest_rate):
+        self.principal_amount = principal_amount
+        self.years = years
+        self.interest_rate = interest_rate
+        self.compounds_per_year = 12
 
+    def part_one_of_equation(self, interest_rate):
+        return round(1 + (self.interest_rate/self.compounds_per_year), 10)
 
+    def part_two_of_equation(self, years):
+        return self.years * self.compounds_per_year
+
+    def compound_interest_calc(self, principal_amount, years, interest_rate):
+        return round(self.principal_amount * pow(self.part_one_of_equation(self.interest_rate), self.part_two_of_equation(self.years)), 2)
 
 # A = P(1 + r/n)nt
 
--- start_code/compound_interest/tests/compound_interest_test.py
+++ start_code/compound_interest/tests/compound_interest_test.py
@@ -4,10 +4,24 @@ from src.compound_interest import CompoundInterest
 
 class CompoundInterestTest(unittest.TestCase):
 
+    def setUp(self):
+        self.calc_1 = CompoundInterest(100, 20, 0.1)
+
     # Tests
 
+    def test_part_one(self):
+        get_part_one = self.calc_1.part_one_of_equation(self.calc_1.interest_rate)
+        self.assertEqual(1.0083333333, get_part_one)
+    
+    def test_part_two(self):
+        get_part_two = self.calc_1.part_two_of_equation(self.calc_1.years)
+        self.assertEqual(240, get_part_two)
+    
     # Should return 732.81 given 100 principal, 10 percent, 20 years
-    def test_compund
+    def test_compound_interest_calc(self):
+        get_compound_interest = self.calc_1.compound_interest_calc(self.calc_1.principal_amount, self.calc_1.years, self.calc_1.interest_rate)
+        self.assertEqual(732.81, get_compound_interest)
+
     # Should return 181.94 given 100 principal, 6 percent, 10 years
 
     # Should return 149,058.55 given 100000 principal, 5 percent, 8 years
